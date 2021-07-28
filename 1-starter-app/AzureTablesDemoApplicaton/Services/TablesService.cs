d32cdf724554450ecd53eb1da39a08e167a9ec96
David Berry <dberry83702@yahoo.com>
2021-07-28 11:30:39 -0500

    Made directories for starter and completed app with files.

---
+++ 1-starter-app/AzureTablesDemoApplicaton/Services/TablesService.cs
@@ -0,0 +1,95 @@
+﻿using AzureTablesDemoApplication.Entities;
+using AzureTablesDemoApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTablesDemoApplication.Services
+{
+    public class TablesService
+    {
+
+        public TablesService()
+        {
+            
+        }
+
+
+        public string[] EXCLUDE_TABLE_ENTITY_KEYS = { "PartitionKey", "RowKey", "odata.etag", "Timestamp" };
+
+
+
+        public IEnumerable<WeatherDataModel> GetAllRows()
+        {
+            return null;
+        }
+
+
+        public IEnumerable<WeatherDataModel> GetFilteredRows(FilterResultsInputModel inputModel)
+        {
+            return null;
+        }
+
+
+        public IEnumerable<WeatherDataModel> MapTableEntityToWeatherDataModel()
+        {
+            return null;
+        }
+
+
+
+        public void InsertTableEntity(WeatherInputModel model)
+        {
+
+        }
+
+
+        public void UpsertTableEntity(WeatherInputModel model)
+        {
+
+        }
+
+
+        public void InsertCustomEntity(WeatherInputModel model)
+        {
+
+        }
+
+
+        public void UpsertCustomEntity(WeatherInputModel model)
+        {
+
+        }
+
+
+        public void InsertExpandableData(string partitionKey, string rowKey, IDictionary<string, string> fields)
+        {
+
+        }
+
+
+        public void UpsertExpandableData(string partitionKey, string rowKey, IDictionary<string, string> fields)
+        {
+
+        }
+
+        public void RemoveEntity(string partitionKey, string rowKey)
+        {
+
+        }
+
+
+        public void InsertBulkData(IEnumerable<WeatherInputModel> items)
+        {
+
+        }
+
+
+        public void UpdateEntity(string partitionKey, string rowKey, IDictionary<string, string> fields)
+        {
+
+        }
+
+    }
+}
---
+++ 2-completed-app/AzureTablesDemoApplicaton/Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTablesDemoApplication.Models
+{
+
+    [Serializable]
+    public class MessageModel
+    {
+        public MessageLevel Level { get; set; }
+
+        public string Message { get; set; }
+    }
+}
