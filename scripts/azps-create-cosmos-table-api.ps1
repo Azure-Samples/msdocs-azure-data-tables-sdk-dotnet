fa79dc2713135aa2b9df27c4640ade831f0ff249
David Berry <dberry83702@yahoo.com>
2021-09-12 23:45:58 -0500

    Adding script for creating Cosmos DB Table API resources with Azure PowerShell

---
+++ scripts/azps-create-cosmos-table-api.ps1
@@ -0,0 +1,28 @@
+$location = 'eastus'
+$resourceGroupName = 'rg-msdocs-tables-sdk-demo'
+$cosmosAccountName = 'cosmos-msdocs-tables-sdk-demo-123'  # Change 123 to a unique three digit value - names must be unique across Azure
+$cosmosTableName = 'WeatherData'
+
+# Create a resource group
+New-AzResourceGroup `
+    -Location $location `
+    -Name $resourceGroupName
+
+# Create an Azure Cosmos DB 
+New-AzCosmosDBAccount `
+    -Name $cosmosAccountName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -ApiKind "Table"
+
+# Create the table for the application to use
+New-AzCosmosDBTable `
+    -Name $cosmosTableName `
+    -AccountName $cosmosAccountName `
+    -ResourceGroupName $resourceGroupName
+
+# This gets the primary Table connection string  
+ $(Get-AzCosmosDBAccountKey `
+    -ResourceGroupName $resourceGroupName `
+    -Name $cosmosAccountName `
+    -Type "ConnectionStrings")."Primary Table Connection String"
